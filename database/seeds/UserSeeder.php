e738e035ae6361616089844b74472583019fadf8
Tommaso <tommaso.lodesani@gmail.com>
2022-07-25 17:03:42 +0200

    user seed

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -11,6 +13,27 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $users = [
+            [
+                'name'      => 'asdf',
+                'email'     => 'asdf@asdf.asdf',
+                'password'  => Hash::make('asdf'),
+            ],
+            [
+                'name'      => 'qwer',
+                'email'     => 'qwer@qwer.qwer',
+                'password'  => Hash::make('qwer'),
+            ],
+            [
+                'name'      => 'zxcv',
+                'email'     => 'zxcv@zxcv.zxcv',
+                'password'  => Hash::make('zxcv'),
+            ],
+
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
     }
 }
